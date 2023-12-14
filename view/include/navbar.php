311a52abc3c72f699433fe1a9f9fd234bfc03442
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-14 16:31:07 +0100

    Create navbar.php

---
+++ view/include/navbar.php
@@ -0,0 +1,59 @@
+<nav class="bg-white border-b border-gray-300">
+        <div class="flex justify-between items-center px-9">
+            <!-- Ícono de Menú -->
+            <button id="menu-button" class="lg:hidden">
+                <i class="fas fa-bars text-cyan-500 text-lg"></i>
+            </button>
+            <!-- Logo -->
+            <!-- <div class="ml-1">
+                <img src="22072231_6563382.jpg" alt="logo" class="h-20 w-28">
+            </div> -->
+
+            <!-- Ícono de Notificación y Perfil -->
+            <div class="space-x-4">
+                <button>
+                    <i class="fas fa-bell text-cyan-500 text-lg"></i>
+                </button>
+
+                <!-- Botón de Perfil -->
+                <button>
+                    <i class="fas fa-user text-cyan-500 text-lg"></i>
+                </button>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Barra lateral -->
+    <div id="sidebar" class="lg:block hidden bg-white w-64 h-screen fixed rounded-none border-none">
+        <!-- Items -->
+        <div class="p-4 space-y-4">
+            <!-- Inicio -->
+            <a href="../Ticket/home.php" aria-label="dashboard" class="relative px-4 py-3 flex items-center space-x-4 rounded-lg text-black bg-gradient-to-r from-sky-600 to-cyan-400">
+                <i class="fas fa-home text-white"></i>
+                <span class="-mr-1 font-medium">Home</span>
+            </a>
+            
+            </a>
+            <a href="./add.php" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group">
+                <i class="fas fa-store"></i>
+                <span>Ajouter ticket</span>
+            </a>
+
+            <a href="../Ticket/mes_tickets.php" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group">
+                <i class="fas fa-wallet"></i>
+                <span>Mes tickets</span>
+            </a>
+            <!-- <a href="#" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group">
+                <i class="fas fa-exchange-alt"></i>
+                <span>Transacciones</span>
+            </a> -->
+            <a href="#" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group">
+                <i class="fas fa-user"></i>
+                <span>Mon compte</span>
+            </a>
+            <a href="#" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Log out</span>
+        </a>
+        </div>
+    </div>
