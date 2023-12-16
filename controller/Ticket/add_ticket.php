ed8fbde57e3e1451bf70b92c87fc1d05d5d9e287
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 17:48:02 +0100

    Update add_ticket.php

--- controller/Ticket/add_ticket.php
+++ controller/Ticket/add_ticket.php
@@ -5,22 +5,21 @@ require_once "../../model/Ticket.php";
 $userModel = new UserModel();
 $ticketModel = new TicketModel();
 
-$priorite = isset($_POST['priorite']) ? $_POST['priorite'] : 'normal';
+$priorite = "";
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $titre = $_POST['titre'];
     $description = $_POST['description'];
-
-    // Modification de l'appel à add_ticket
-    if ($ticketModel->add_ticket($titre, $description, $priorite))  {
-        header('Location: ../../view/Ticket/home.php');
-        exit();
-    } else {
-        header('Location: ../../view/Ticket/add.php ?STATUS=probleme de ajout');
-    }
-
-    // Définir $priorite avec la valeur soumise dans le formulaire
     $priorite = $_POST['priorite'];
+
+    $selectedUsers = isset($_POST['selected_users']) ? $_POST['selected_users'] : [];
+
+    if ($ticketModel->addTicketAndAssign($titre, $description, $priorite, $selectedUsers)) {
+        header("Location: ../../view/Ticket/home.php?STATUS=ticket ajoute avec secces");
+    } else {
+        header("Location: ../../view/Ticket/add.php?STATUS=echec d'ajout");
+    }
+    
 }
 
 $users = $userModel->getAllUsers();
