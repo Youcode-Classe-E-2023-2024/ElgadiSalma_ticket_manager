6f6b77d3d2fab2988407f4dc1804e0afa71fc014
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 21:43:13 +0100

    Update home.php

--- view/Ticket/home.php
+++ view/Ticket/home.php
@@ -65,7 +65,7 @@
 </head>
 <body class="bg-indigo-200	 lg:flex">
     
-    <?php require_once "../include/navbar.php"; ?>
+    <?php require_once "../../view/include/navbar.php"; ?>
     
     <div class="lg:w-full lg:ml-64 px-6 py-8 flex flex-col	">
 
@@ -91,34 +91,19 @@
     <div class="container m-auto px-6 text-gray-500 md:px-12 flex flex-wrap	 xl:px-0">
         <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
             <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
-                </div>
-                <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg" class="w-2/3 ml-auto -mb-12" alt="illustration" loading="lazy" width="900" height="600">
-            </div>
-            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">UI Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
-                </div>
-                <img src="https://tailus.io/sources/blocks/end-image/preview/images/ui-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
-            </div>
-            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">UI Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
-                </div>
-                <img src="https://tailus.io/sources/blocks/end-image/preview/images/ui-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
-            </div>
-            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">UX Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
+            <div class="mb-12 space-y-4">
+    <?php
+    if (!empty($tickets)) {
+        foreach ($tickets as $ticket) {?>
+                        <h3 class="text-2xl font-semibold text-purple-900">UX <?php echo $ticket['titre']; ?></h3>
+                        <p class="mb-6"><?php echo $ticket['description']; ?></p>
+                        <a href="#" class="block font-medium text-purple-600">Know more</a>
+                <?php
+                    }
+                } else {
+                    echo "Aucun ticket disponible.";
+                }
+                ?>
                 </div>
                 <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto " alt="illustration" loading="lazy" width="900" height="600">
             </div>
