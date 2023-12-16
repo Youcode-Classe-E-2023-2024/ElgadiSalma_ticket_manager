670f0f37116afc3ea80d2bb1c3f197819771a7ca
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 15:28:11 +0100

    Update add_ticket.php

--- controller/Ticket/add_ticket.php
+++ controller/Ticket/add_ticket.php
@@ -2,20 +2,28 @@
 require_once "../../model/User.php";
 require_once "../../model/Ticket.php";
 
-// $userModel = new UserModel();
+$userModel = new UserModel();
 $ticketModel = new TicketModel();
 
-$titre = $_POST['titre'];
-$description = $_POST['description'];
-$priorite = $_POST['priorite'];
+$priorite = isset($_POST['priorite']) ? $_POST['priorite'] : 'normal';
 
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $titre = $_POST['titre'];
+    $description = $_POST['description'];
 
+    // Modification de l'appel à add_ticket
+    if ($ticketModel->add_ticket($titre, $description, $priorite))  {
+        header('Location: ../../view/Ticket/home.php');
+        exit();
+    } else {
+        header('Location: ../../view/Ticket/add.php ?STATUS=probleme de ajout');
+    }
 
-// Modification de l'appel à add_ticket
-if ($ticketModel->add_ticket($titre, $description, $priorite)) {
-    echo 'zz';
+    // Définir $priorite avec la valeur soumise dans le formulaire
+    $priorite = $_POST['priorite'];
 }
 
 $users = $userModel->getAllUsers();
-include_once "../../view/Ticket/add.php";   
+
+include_once "../../view/Ticket/add.php";
 ?>
