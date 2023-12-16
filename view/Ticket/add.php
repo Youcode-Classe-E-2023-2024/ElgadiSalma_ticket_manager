10e99c03511bfeefb7d5d1badc1db5476e1df0a0
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 14:10:12 +0100

    Update add.php

--- view/Ticket/add.php
+++ view/Ticket/add.php
@@ -11,61 +11,53 @@
 <?php require_once "../../view/include/navbar.php"; ?>
     <body>
 
-        
-
-        
-            <div class="w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 p-4 flex items-center justify-center" >
-                <div class="bg-white py-6 px-10 sm:max-w-md w-full ">
-            <!-- <div class="sm:text-3xl text-2xl font-semibold text-center text-sky-600  mb-12">
-                Registration Form 
-            </div> -->
-            <form action="">
-            <div>
-                 <input type="text" name="titre" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8"  placeholder="Titre "/>
-            </div>
-
-             <div>
-                 <input type="text" name="description" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 "  placeholder="Description"/>
-            </div>
-            <div class="">
-
-            <select name="Priorite"  class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8">
-                <option selected disabled>priorite</option>
-                <option <?php echo ($priorite['priorite'] == 'normal') ? 'selected' : ''; ?>>Normal</option>
-                <option <?php echo ($priorite['priorite'] == 'urgent') ? 'selected' : ''; ?>>Urgent</option>
-            </select>
-            </div>
-
-            <div class="relative inline-block">
-            <input type="text" id="searchInput" placeholder="Search" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500">
-            <div class="absolute max-h-40 w-full z-10 mt-2 bg-white border border-gray-300 rounded-md shadow-lg overflow-y-auto hidden"  id="dropdownContent">
-                <?php
-                if (!empty($users)) {
-                    foreach ($users as $user) {
-                    ?>
-                        <div class="p-2">
-                            <label><input type="checkbox" value="<?php echo $user['id_user']; ?>" class="mr-2"><?php echo $user['username']; ?></label>
-                        </div>
-                    <?php
-                    }
-                } else {
-                    echo "Aucun utilisateur disponible."; // Message à afficher si $users est vide
-                }
-                ?>
-            </div>
-            </div>
-            <button type="button" onclick="toggleDropdown()"><></button>
-
-            
-            <div class="flex justify-center my-6">
-                <button class=" rounded-full  p-3 w-full sm:w-56   bg-gradient-to-r from-sky-600  to-teal-300 text-white text-lg font-semibold " >
-                    Créer Ticket
-                </button>
-            </div>
-            
-        </form>
-            
+        <div class="w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 p-4 flex items-center justify-center" >
+            <div class="bg-white py-6 px-10 sm:max-w-md w-full ">
+        <form action="../../controller/Ticket/add_ticket.php" method="post">
+        <div>
+                <input type="text" name="titre" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8"  placeholder="Titre " required/>
         </div>
+
+            <div>
+                <input type="text" name="description" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 "  placeholder="Description"required/>
+        </div>
+        <div class="">
+            <select name="priorite" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8" required>
+                <option selected disabled>priorite</option>
+                <option <?php echo ($priorite == 'normal') ? 'selected' : ''; ?>>Normal</option>
+                <option <?php echo ($priorite == 'urgent') ? 'selected' : ''; ?>>Urgent</option>
+            </select>
+        </div>
+        <div class="relative inline-block">
+        <input type="text" id="searchInput" placeholder="Search" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500">
+        <div class="absolute max-h-40 w-full z-10 mt-2 bg-white border border-gray-300 rounded-md shadow-lg overflow-y-auto hidden"  id="dropdownContent">
+            <?php
+            if (!empty($users)) {
+                foreach ($users as $user) {
+                ?>
+                    <div class="p-2">
+                        <label><input type="checkbox" value="<?php echo $user['id_user']; ?>" class="mr-2"><?php echo $user['username']; ?></label>
+                    </div>
+                <?php
+                }
+            } else {
+                echo "utilisateurs indisponibles";
+            }
+            ?>
+        </div>
+        </div>
+        <button type="button" onclick="toggleDropdown()"><></button>
+
+        
+        <div class="flex justify-center my-6">
+            <button type="submit" class=" rounded-full  p-3 w-full sm:w-56   bg-gradient-to-r from-sky-600  to-teal-300 text-white text-lg font-semibold " >
+                Créer Ticket
+            </button>
+        </div>
+        
+    </form>
+        
+    </div>
     </div>
 </div>
 
