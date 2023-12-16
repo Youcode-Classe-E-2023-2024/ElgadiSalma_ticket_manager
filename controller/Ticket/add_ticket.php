667f3aec8c1bb61e868ce3cd77ef2f117a211d95
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 14:10:20 +0100

    Update add_ticket.php

--- controller/Ticket/add_ticket.php
+++ controller/Ticket/add_ticket.php
@@ -1,10 +1,21 @@
 <?php
 require_once "../../model/User.php";
+require_once "../../model/Ticket.php";
 
-$userModel = new UserModel();
+// $userModel = new UserModel();
+$ticketModel = new TicketModel();
 
+$titre = $_POST['titre'];
+$description = $_POST['description'];
+$priorite = $_POST['priorite'];
+
+
+
+// Modification de l'appel à add_ticket
+if ($ticketModel->add_ticket($titre, $description, $priorite)) {
+    echo 'zz';
+}
 
 $users = $userModel->getAllUsers();
-
-include_once "../../view/Ticket/add.php";
+include_once "../../view/Ticket/add.php";   
 ?>
