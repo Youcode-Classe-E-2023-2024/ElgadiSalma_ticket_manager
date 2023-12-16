7b4b5bda6354682ff158dba3aded2869e73c8fc0
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-16 17:47:53 +0100

    Update Ticket.php

--- model/Ticket.php
+++ model/Ticket.php
@@ -1,35 +1,44 @@
 <?php
 include_once '../../config/Database.php';
 
-
-Class TicketModel{
+class TicketModel
+{
     private $db;
 
     public function __construct()
     {
         $this->db = new Database();
-
     }
 
-    public function add_ticket($titre, $description, $priorite) {
+    public function addTicketAndAssign($titre, $description, $priorite, $selectedUsers)
+    {
+        $this->db->beginTransaction();
+
         $this->db->query("INSERT INTO ticket (titre, description, priorite) VALUES (:titre, :description, :priorite)");
         $this->db->bind(':titre', $titre);
         $this->db->bind(':description', $description);
         $this->db->bind(':priorite', $priorite);
-    
-        if ($this->db->execute()) {
-            return true;
-        } else {
+
+        if (!$this->db->execute()) {
+            $this->db->cancelTransaction();
             return false;
         }
-    }   
 
-    // public function getPriorite() {
-    //     // Assurez-vous d'adapter cette requête en fonction de votre modèle de données
-    //     $this->db->query("SELECT priorite FROM user WHERE id = :user_id");
-    //     $this->db->bind(':user_id', $_SESSION['user_id']);
-    //     $result = $this->db->single();
-    
-    //     return ($result) ? $result['priorite'] : null;
-    // }
+        $ticketId = $this->db->lastInsertId();
+
+        foreach ($selectedUsers as $userId) {
+            $this->db->query("INSERT INTO assigne (id_t, id_u) VALUES (:id_ticket, :id_user)");
+            $this->db->bind(':id_ticket', $ticketId);
+            $this->db->bind(':id_user', $userId);
+
+            if (!$this->db->execute()) {
+                $this->db->cancelTransaction();
+                return false;
+            }
+        }
+
+        $this->db->endTransaction();
+        return true;
+    }
 }
+?>
