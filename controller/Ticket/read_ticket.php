2cf3a1cda16786e7a089daf94e40ac16a6c3d63d
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-18 00:55:44 +0100

    Update read_ticket.php

--- controller/Ticket/read_ticket.php
+++ controller/Ticket/read_ticket.php
@@ -4,6 +4,6 @@ require_once "../../model/Ticket.php";
 $ticketModel = new TicketModel();
 $tickets = $ticketModel->getAllTickets();
 
-// var_dump($tickets);
-// include_once '../../view/Ticket/home.php';
+
+
 ?>
