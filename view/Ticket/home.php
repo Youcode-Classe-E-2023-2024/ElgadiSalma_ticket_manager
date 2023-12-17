ba17478d0373b93f7b6c6b28ed1e75c6f695a759
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-17 11:39:45 +0100

    Update home.php

--- view/Ticket/home.php
+++ view/Ticket/home.php
@@ -6,6 +6,7 @@
 <title>avito</title>
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+<script src="../../js/main.js"></script>
     <style>
         select {
             -webkit-appearance: none;
@@ -71,7 +72,7 @@
 
     <h1 class="text-4xl text-purple-500 py-4 text-center font-bold">Tous les tickets</h1>
 
-    <div class="relative mt-6 max-w-lg py-8 text-gray-900 mx-auto flex gap-x-3">
+    <div class="relative mt-6 max-w-2xl	 py-8 text-gray-900 mx-auto flex gap-x-3">
         <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
             <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
                 <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -82,9 +83,21 @@
     
 
     <div class="select">
-        <select name="category" id="category">
-            <option value="all" selected>Category</option>
-                <option value="">zz</option>
+        <select name="priorite" id="priorite">
+        <option selected disabled>Priorité</option>
+            <?php include_once '../../controller/Ticket/add_ticket.php';?>
+            <option value="normal" <?php echo ($priorite == 'normal') ? 'selected' : ''; ?>>Normal</option>
+            <option value="urgent" <?php echo ($priorite == 'urgent') ? 'selected' : ''; ?>>Urgent</option>
+        </select>
+    </div>
+
+    <div class="select">
+        <select name="statut" id="statut">
+        <option selected disabled>Statut</option>
+            <?php include_once '../../controller/Ticket/read_ticket.php';?>
+            <option value="normal" <?php echo ($statut == 'To Do') ? 'selected' : ''; ?>>To Do</option>
+            <option value="urgent" <?php echo ($statut == 'Doing') ? 'selected' : ''; ?>>Doing</option>
+            <option value="normal" <?php echo ($statut == 'Done') ? 'selected' : ''; ?>>Done</option>
         </select>
     </div>
 </div>
@@ -93,6 +106,7 @@
             <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
             <div class="mb-12 space-y-4">
     <?php
+    include_once '../../controller/Ticket/read_ticket.php';
     if (!empty($tickets)) {
         foreach ($tickets as $ticket) {?>
                         <h3 class="text-2xl font-semibold text-purple-900">UX <?php echo $ticket['titre']; ?></h3>
@@ -114,16 +128,6 @@
 <!-- </div> -->
 </body>
 
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var menuButton = document.getElementById('menu-button');
-        var sidebar = document.getElementById('sidebar');
 
-        menuButton.addEventListener('click', function() {
-            sidebar.classList.toggle('hidden');
-            sidebar.classList.toggle('lg:block');
-        });
-    });
-</script>
 
 </html>
