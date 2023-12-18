4721538e5c831e1ba9693e72e93d1f1f402272ab
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-18 10:52:04 +0100

    Update filter_tickets.php

--- controller/Ticket/filter_tickets.php
+++ controller/Ticket/filter_tickets.php
@@ -6,26 +6,35 @@ $ticketModel = new TicketModel();
 
 $priorite = isset($_GET['priorite']) ? $_GET['priorite'] : 'all';
 $statut = isset($_GET['statut']) ? $_GET['statut'] : 'all';
+$assignedTo = isset($_GET['assignedTo']) ? $_GET['assignedTo'] : 'all';
 
 switch (true) {
-    case ($priorite == 'all' && $statut == 'all'):
+    case ($priorite == 'all' && $statut == 'all' && $assignedTo == 'all'):
         $filteredTickets = $ticketModel->getAllTickets();
         break;
 
-    case ($priorite != 'all' && $statut == 'all'):
+    case ($priorite != 'all' && $statut == 'all' && $assignedTo == 'all'):
         $filteredTickets = $ticketModel->filterPriorite($priorite);
         break;
 
-    case ($priorite == 'all' && $statut != 'all'):
+    case ($priorite == 'all' && $statut != 'all' && $assignedTo == 'all'):
         $filteredTickets = $ticketModel->filterStatut($statut);
         break;
 
-    case ($priorite != 'all' && $statut != 'all'):
+    case ($priorite != 'all' && $statut != 'all' && $assignedTo == 'all'):
         $filteredTickets = $ticketModel->filterPrioriteStatut($priorite, $statut);
         break;
+
+    case ($priorite == 'all' && $statut == 'all' && $assignedTo != 'all'):
+        $filteredTickets = $ticketModel->filterAssignedTo($assignedTo);
+        break;
+
+    // Ajoutez d'autres cas selon vos besoins...
+
+    default:
+        // Cas par défaut : tous les filtres sont à 'all'
+        $filteredTickets = $ticketModel->getAllTickets();
 }
 
-// Renvoie les tickets filtrés au format JSON
-header('Content-Type: application/json');
 echo json_encode($filteredTickets);
 ?>
