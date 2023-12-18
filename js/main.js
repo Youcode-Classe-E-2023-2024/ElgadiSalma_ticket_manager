6949000d2cd695f25305f4662e72318614fa01f1
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-18 21:28:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var prioriteSelect = document.getElementById('priorite');
     var statutSelect = document.getElementById('statut');
+    var assignedToSelect = document.getElementById('assignedTo');
     var searchInput = document.getElementById('searchInput');
 
     prioriteSelect.addEventListener('change', fetchDataAndDisplay);
     statutSelect.addEventListener('change', fetchDataAndDisplay);
+    assignedToSelect.addEventListener('change', fetchDataAndDisplay);
     searchInput.addEventListener('keyup', Search);
 
     fetchDataAndDisplay();
@@ -14,10 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
     function fetchDataAndDisplay() {
         var priorite = prioriteSelect.value;
         var statut = statutSelect.value;
+        var assignedTo = assignedToSelect.value;
 
-        console.log('Fetching data  - Priorite:', priorite, 'Statut:', statut);
+        console.log('Fetching data - Priorite:', priorite, 'Statut:', statut, 'Assigned To:', assignedTo);
 
-        fetch(`../../controller/Ticket/filter_tickets.php?priorite=${priorite}&statut=${statut}`)
+        fetch(`../../controller/Ticket/filter_tickets.php?priorite=${priorite}&statut=${statut}&assignedTo=${assignedTo}`)
             .then(response => response.json())
             .then(tickets => {
                 ticketData = tickets;
@@ -38,16 +41,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const rows = tickets.map((ticket) => {
             return (
                 `
-                    
-                        <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                            <div class="mb-12 space-y-4">
-                                <h3 class="text-2xl font-semibold text-purple-900">UX ${ticket.titre}</h3>
-                                <p class="mb-6 ">${ticket.description}</p>
-                                <a href="#" class="block font-medium text-purple-600">Know more</a>
-                            </div>
-                            <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
+                    <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+                        <div class="mb-12 space-y-4">
+                            <h3 class="text-2xl font-semibold text-purple-900">UX ${ticket.titre}</h3>
+                            <p class="mb-6 ">${ticket.description}</p>
+                            <a href="#" class="block font-medium text-purple-600">Know more</a>
                         </div>
-                    
+                        <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
+                    </div>
                 `
             );
         });
