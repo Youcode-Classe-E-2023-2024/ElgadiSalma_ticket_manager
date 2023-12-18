ff8620a83f3a42013d108342242c0c553ef141ac
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-18 21:28:34 +0100

    Update add_ticket.php

--- controller/Ticket/add_ticket.php
+++ controller/Ticket/add_ticket.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require_once "../../model/User.php";
 require_once "../../model/Ticket.php";
 
@@ -8,14 +10,17 @@ $ticketModel = new TicketModel();
 $priorite = "";
 $statut = "";
 
+
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $titre = $_POST['titre'];
     $description = $_POST['description'];
     $priorite = $_POST['priorite'];
+    $created_by = $_SESSION['username'];
 
     $selectedUsers = isset($_POST['selected_users']) ? $_POST['selected_users'] : [];
+    $selectedTags = isset($_POST['selected_tags']) ? $_POST['selected_tags'] : [];
 
-    if ($ticketModel->addTicketAndAssign($titre, $description, $priorite, $selectedUsers)) {
+    if ($ticketModel->addTicketAndAssign($titre, $description, $priorite,$created_by, $selectedUsers ,$selectedTags)) {
         header("Location: ../../view/Ticket/home.php?STATUS=ticket ajoute avec secces");
     } else {
         header("Location: ../../view/Ticket/add.php?STATUS=echec d'ajout");
@@ -24,6 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
 }
 
 $users = $userModel->getAllUsers();
+$tags = $userModel->getAllTags();
 
 // include_once "../../view/Ticket/add.php";
 ?>
