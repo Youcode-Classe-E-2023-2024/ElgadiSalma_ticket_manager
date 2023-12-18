3d449a5aec482223e9d0fc4e03c790b43ee8a57c
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-18 01:21:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,18 +37,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const rows = tickets.map((ticket) => {
             return (
-                `<div class="container m-auto px-6 text-gray-500 md:px-12 flex flex-wrap xl:px-0">
-                    <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
+                `
+                    
                         <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
                             <div class="mb-12 space-y-4">
                                 <h3 class="text-2xl font-semibold text-purple-900">UX ${ticket.titre}</h3>
-                                <p class="mb-6">${ticket.description}</p>
+                                <p class="mb-6 ">${ticket.description}</p>
                                 <a href="#" class="block font-medium text-purple-600">Know more</a>
                             </div>
                             <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
                         </div>
-                    </div>
-                </div>`
+                    
+                `
             );
         });
 
