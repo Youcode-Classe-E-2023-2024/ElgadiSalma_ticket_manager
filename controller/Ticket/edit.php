2cacd13a93de25cf8cf1fb536b817301c7e58056
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-19 22:04:09 +0100

    Update edit.php

--- controller/Ticket/edit.php
+++ controller/Ticket/edit.php
@@ -1,4 +1,26 @@
 <?php
 require_once "../../model/Ticket.php";
 $ticketModel = new TicketModel();
-$statut = $_POST['statut'];
+
+
+
+
+    if (isset($_POST['submit'])) {
+        // echo'zz';
+        $id_ticket = $_POST['id'];
+        $statut = $_POST['statut'];
+        // $tickets = $ticketModel->getTicketsById($id_ticket);
+        // $ticketModel->updateTicket($statut ,$id_ticket);
+        // var_dump($id_ticket, $statut);  
+
+        if($ticketModel->updateTicket($statut ,$id_ticket)) {
+           
+        
+            header("Location:../../view/Ticket/mes_tickets.php?éditee avec succcess");
+        }
+        else {
+            header("Location:../../view/Ticket/mes_tickets.php?error");
+        }
+    }
+
+
