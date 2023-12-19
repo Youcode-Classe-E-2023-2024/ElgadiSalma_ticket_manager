b10e3a6a2ec3eeca9e7f0a9908e6eb6309e2a05a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-19 23:11:50 +0100

    Update description.php

--- view/Ticket/description.php
+++ view/Ticket/description.php
@@ -50,6 +50,37 @@ if (!isset($_SESSION['id_user'])) {
                     </svg>
                 </div>
                 <?php endforeach; ?>
+
+                <div class="my-8 flex-col">
+                    <div class="flex justify-between">
+                        <h1><ins>Commentaires :</ins></h1>
+                        <!-- <button class="py-1 px-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Ajouter commentaire</button> -->
+                    </div>
+                    <?php
+                require_once "../../controller/Ticket/read_ticket.php";
+                foreach ($commentaires as $commentaire):
+                ?>
+                    <p><?php echo $commentaire['text']; ?></p>
+                    <p class="md:text-sm"><?php echo $commentaire['date_com']; ?></p>
+                <?php endforeach; ?>
+
+                <form action="../../controller/Ticket/add_commentaire.php" method="post">
+                <input type="hidden" name="id" value="<?php echo $ticket['id_ticket']; ?>">
+                    <div class="flex gap-2">
+
+                <div class="relative h-10 w-full min-w-[200px]">
+                <input
+                class="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+                />
+                <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Ajouter Commentaire
+                </label>
+                </div>
+                <button type="submit" name="submit" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Save</button></div>
+                </form>
+                </div>
+
                 </form>
             </div>
         </div>
