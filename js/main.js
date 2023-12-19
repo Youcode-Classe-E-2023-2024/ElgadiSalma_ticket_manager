8533146c339a07128624f12d3a1a7d3c35d271e2
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-19 01:37:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <div class="mb-12 space-y-4">
                             <h3 class="text-2xl font-semibold text-purple-900">UX ${ticket.titre}</h3>
                             <p class="mb-6 ">${ticket.description}</p>
-                            <a href="./description.php" class="block font-medium text-purple-600">Know more</a>
+                            <a href="./description.php?id=${ticket.id_ticket}" class="block font-medium text-purple-600">Know more</a>
                         </div>
                         <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
                     </div>
