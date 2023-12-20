ef49261b799c73e33dd2d6be197b820cf1c49107
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-20 19:04:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,63 @@
-<?php
-function
-dd($var){
-    echo '<pre>';
-    echo '<code>';
-    print_r($var);
-    echo '</code>';
-    echo '</pre>';
-    die();
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avito</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div>
+    <section class="relative flex flex-col h-screen items-center max-w-lg px-4 sm:pt-24 pt-12 sm:pb-8 mx-auto text-left md:max-w-none md:text-center">
 
+    <div class="text-left">
 
-if(!isset($_GET['page'])){
-    $_GET['page'] = 'home';
-}
+      
+        <div class="relative items-center  mx-auto  lg:px-16 max-w-6xl">
+          <div class="relative flex-col items-start m-auto align-middle">
+            <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+              <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
+                <div class="max-w-xl text-center lg:text-left">
+                  <div>
 
-$page = $_GET['page'];
+                    <p class="text-3xl font-semibold tracking-tight text-[#201515] sm:text-5xl">
+                      Space Management Software
+                    </p>
+                    <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
+                      Use this paragraph to share information about your company or products. Make it engaging and interesting, and showcase your brand's personality. Thanks for visiting our website!
+                    </p>
+                  </div>
+                  <div class="flex justify-center gap-3 mt-10 lg:justify-start">
+                    <a class="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600" href="#">
+                      <span> Read more &nbsp; → </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="order-first block  mt-12 aspect-square lg:mt-0">
+                <img class="object-cover rounded-3xl object-center w-full mx-auto bg-gray-300 lg:ml-auto " alt="hero" src="https://i.pinimg.com/originals/2e/2b/21/2e2b21aeed393403d4620367f9e093f9.gif" />
+              </div>
+            </div>
+          </div>
+        
+        </div>
+        <div class="max-w-lg px-4 pb-24 mx-auto text-left md:max-w-none md:text-center">
+        <div class='text-center py-4 space-x-4'>
+            <a href="./view/User/connect.php">
+          <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-[#FF4F01] border border-[#E2E8F0] translate-y-1 text-white hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
+            <span> Log in </span>
+          </button>
+        </a>
 
-$all_pages = scandir('controller');
-
-
-
-if(in_array($page . '_controller.php' , $all_pages)){
-
-    include_once 'controller/' . $page . '_controller.php' ;
-}
-else{
-    header("location:notfound.html");
-}
+          <a href="./view/User/register.php">
+            <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-white border border-[#E2E8F0] translate-y-1 text-[#16161d] hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
+            <span> Register</span>
+          </button>
+        </a>
+          
+        </div>
+      </div>
+      </section>
+</div>
+</body>
+</html>
