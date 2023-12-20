6e9d4cefabf2c80d1e7a3a4c73085591e6342e62
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-20 17:06:38 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,10 +41,10 @@ document.addEventListener('DOMContentLoaded', function () {
         const rows = tickets.map((ticket) => {
             return (
                 `
-                    <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12">
+                    <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12" style="width: 30%;" >
                         <div class="mb-12 space-y-4">
                             <h3 class="text-2xl font-semibold text-purple-900"> ${ticket.titre}</h3>
-                            <p class="mb-6 ">${ticket.description}</p>
+                            <p class="mb-6 whitespace-pre">${ticket.description}</p>
                             <a href="./description.php?id=${ticket.id_ticket}" class="block font-medium text-purple-600">Know more</a>
                         </div>
                         <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
