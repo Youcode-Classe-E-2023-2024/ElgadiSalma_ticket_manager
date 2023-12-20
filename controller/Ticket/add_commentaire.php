c55d668a31d35e1d8e82c79b86c0f4dc8194a1af
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-20 09:30:02 +0100

    Update add_commentaire.php

--- controller/Ticket/add_commentaire.php
+++ controller/Ticket/add_commentaire.php
@@ -3,16 +3,15 @@ require_once "../../model/Ticket.php";
 $ticketModel = new TicketModel();
 
 
-
-
     if (isset($_POST['submit'])) {
+        $id_user=$_POST['id_user'];
         $id_ticket = $_POST['id'];
         $text = $_POST['text'];
+        var_dump($id_user, $id_ticket, $text);
     
-        if($ticketModel->addCommentaire($text ,$id_ticket)) {
-           
-        
-            header("Location:../../view/Ticket/description.php?ajoutee avec succcess");
+        if($ticketModel->addCommentaire($id_user, $id_ticket, $text)) {
+           echo 'zz';
+            header("Location:../../view/Ticket/home.php?ajoutee avec succcess");
         }
         else {
             header("Location:../../view/Ticket/home.php?error");
